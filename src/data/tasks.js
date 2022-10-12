9ec72d0b28709636d5dc0c495e812ececb26d92e
naldowebdeveloper <ednaldo_wk@hotmail.com>
2022-10-12 18:48:28 -0300

    first-commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Lorem ipsum dolor",
+        description: "Lorem ipsum dolor Lorem ipsum dolor"
+    },
+    {
+        id: 1,
+        title: "Lorem ipsum dolor",
+        description: "Lorem ipsum dolor Lorem ipsum dolor"
+    },
+    {
+        id: 2,
+        title: "Lorem ipsum dolor",
+        description: "Lorem ipsum dolor Lorem ipsum dolor"
+    }
+]
